1f1c84b887ff0e425e047a36db4b71b10369111d
Michael Evans <77376183+Unloosed@users.noreply.github.com>
2024-11-04 08:20:17 -0800

    Update utils.py

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,12 +1,23 @@
 import random
+import sys
+
+import pygame
 
 from scripts.button import Button
 from scripts.canvas import Canvas
 
 
-def generate_random_button(canvas: Canvas, image_path: str, scale: float) -> Button:
-    position = [random.randint(0, canvas.dimensions[0] - 100), random.randint(0, canvas.dimensions[1] - 50)]
-    velocity = [random.randint(-5, 5), random.randint(-5, 5)]  # This can return [0, 0]
-    while velocity == [0, 0]:  # Ensure buttons have non-zero velocity
-        velocity = [random.randint(-5, 5), random.randint(-5, 5)]
+def generate_random_button(canvas: Canvas, image_path: str, scale: float, moving_button: bool = True) -> Button:
+    # Offset the bounds of the randint() call so that button doesn't get stuck at the bottom/left side
+    position = [random.randint(0, canvas.dimensions[0] - 100), random.randint(0, canvas.dimensions[1] - 100)]
+    if moving_button:
+        velocity = [random.randint(-5, 5), random.randint(-5, 5)]  # This can return [0, 0]
+        while velocity == [0, 0]:  # Ensure button has non-zero velocity
+            velocity = [random.randint(-5, 5), random.randint(-5, 5)]
+    else:
+        velocity = [0, 0]
     return Button(canvas=canvas, image_path=image_path, position=position, scale=scale, velocity=velocity)
+
+def quit_game() -> None:
+    pygame.quit()
+    sys.exit(0)
