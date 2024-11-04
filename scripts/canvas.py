35b54f84d303b4ca3f1d8e035a04d5b46bfb9b2b
Michael Evans <77376183+Unloosed@users.noreply.github.com>
2024-11-03 18:05:00 -0800

    Create canvas.py

---
+++ scripts/canvas.py
@@ -0,0 +1,20 @@
+from typing import Tuple
+
+import pygame
+
+
+class Canvas:
+    def __init__(self, dimensions: Tuple[int, int] = (1280, 720), title: str = "Game"):
+        self.dimensions = dimensions
+        self.screen = pygame.display.set_mode(dimensions)
+        pygame.display.set_caption(title)
+        self.print_dimensions()
+
+    def print_dimensions(self):
+        print(f'Canvas Dimensions: {self.dimensions}')
+
+    def fill(self, color: Tuple[int, int, int] = (255, 255, 255)):
+        self.screen.fill(color)
+
+    def update(self):
+        pygame.display.flip()
