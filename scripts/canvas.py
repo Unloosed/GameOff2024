bd4c685ba893904bd4f44b66e776e3f34634e62d
Michael Evans <77376183+Unloosed@users.noreply.github.com>
2024-11-04 15:00:34 -0800

    Update canvas.py

--- scripts/canvas.py
+++ scripts/canvas.py
@@ -3,18 +3,24 @@ from typing import Tuple
 import pygame
 
 
+# TODO: Add functionality to scale Canvas from settings.py
+
+
 class Canvas:
-    def __init__(self, dimensions: Tuple[int, int] = (1280, 720), title: str = "I Am Spy"):
+    def __init__(self, dimensions: Tuple[int, int] = (1280, 720), title: str = "I Am Spy",
+                 color: Tuple[int, int, int] = (255, 255, 255)):
         self.dimensions = dimensions
+        self.title = title
+        self.color = color
         self.screen = pygame.display.set_mode(dimensions)
-        pygame.display.set_caption(title)
+        pygame.display.set_caption(self.title)
         self.print_dimensions()
 
-    def print_dimensions(self):
+    def print_dimensions(self) -> None:
         print(f'Canvas Dimensions: {self.dimensions}')
 
-    def fill(self, color: Tuple[int, int, int] = (255, 255, 255)):
-        self.screen.fill(color)
+    def fill(self) -> None:
+        self.screen.fill(self.color)
 
-    def update(self):
+    def update(self) -> None:
         pygame.display.flip()
