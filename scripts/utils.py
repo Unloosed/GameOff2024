87e20c780ca5685c9ba911e74c74e44ef5f3af3d
Michael Evans <77376183+Unloosed@users.noreply.github.com>
2024-11-08 09:09:43 -0800

    Update utils.py
    better randomization to avoid buttons spawning together and therefore getting stuck

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,7 +1,7 @@
 import os
 import random
 import sys
-from typing import Tuple
+from typing import Tuple, List
 
 import pygame
 
@@ -11,12 +11,16 @@ from scripts.canvas import Canvas
 
 # TODO: Maybe reorganize this so that class-specific functions [like generate_random_button()]
 #  are instead defined in the relevant class module
+# TODO: Adjust generate_random_button() to ensure that generated buttons do not overlap other buttons.
+#  while self.image_rect.colliderect(other_button.image_rect):
+#   position = [random.randint(0, canvas.dimensions[0] - 100), random.randint(0, canvas.dimensions[1] - 100)]
 
 
-def generate_random_button(canvas: Canvas, text: str, image_path: str, scale: float, moving_button: bool = True,
-                           generate_sparks_toggle: bool = False) -> Button:
-    # Offset the bounds of the randint() call so that button doesn't get stuck at the bottom/left side
-    position = [random.randint(0, canvas.dimensions[0] - 100), random.randint(0, canvas.dimensions[1] - 100)]
+def generate_random_button(canvas: Canvas, text: str, image_path: str, scale: float, existing_buttons: List[Button], moving_button: bool = True,
+                           generate_sparks_toggle: bool = False, has_collision: bool = False) -> Button:
+    position = create_position(canvas)
+    while any(button.image_rect.colliderect(pygame.Rect(position, (100, 100))) for button in existing_buttons):
+        position = create_position(canvas)
     if moving_button:
         velocity = [random.randint(-5, 5), random.randint(-5, 5)]  # This can return [0, 0]
         while velocity == [0, 0]:  # Ensure button has non-zero velocity
@@ -24,12 +28,17 @@ def generate_random_button(canvas: Canvas, text: str, image_path: str, scale: fl
     else:
         velocity = [0, 0]
     return Button(canvas=canvas, text=text, image_path=image_path, position=position, scale=scale, velocity=velocity,
-                  generate_sparks_toggle=generate_sparks_toggle)
+                  generate_sparks_toggle=generate_sparks_toggle, has_collision=has_collision)
 
 
 def quit_game() -> None:
     pygame.quit()
     sys.exit(0)
+
+
+def create_position(canvas: Canvas):
+    # Offset the bounds of the randint() call so that button doesn't get stuck at the bottom/left side
+    return [random.randint(0, canvas.dimensions[0] - 100), random.randint(0, canvas.dimensions[1] - 100)]
 
 
 def get_random_image(image_directory: str, return_text: bool = False) -> Tuple[str, str]:
